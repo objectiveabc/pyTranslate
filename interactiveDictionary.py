ab479c89caab001800fdffa18cd88768c9f23b1d
objectiveabc <garysfw@outlook.com>
2017-12-27 18:34:02 +0000

    Adding the ability to suggest possible words based on input

--- interactiveDictionary.py
+++ interactiveDictionary.py
@@ -1,11 +1,14 @@
-import json
+import json, difflib
+
 data = json.load(open("data.json"))
 
 def find(w):
     if w in data: #conditional to check whether the input is in the data set.
         return data[w]
+    elif difflib.get_close_matches(w, data) != False:
+        print ("Did you mean ", difflib.get_close_matches(w, data))
     else:
         return "Uh-oh! This word isn't currently covered."
 
 word = raw_input("Enter a word: ")
-print(find(word.lower())) #lower() - converts input and makes the query caps insensitive - assumes data.json is lower case
+print(find(word.upper())) #upper() - converts input and makes the query caps insensitive - assumes data.json is lower case
